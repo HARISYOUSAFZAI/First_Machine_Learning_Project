8a4c65fcc08c3c7e8b1f522a802877fb778b5884
HARISYOUSAFZAI <HARISALI.510@GMAIL.COM>
2023-06-15 13:52:07 +0300

    updated README and build log and exception packages

---
+++ housing/entity/__init__.py
@@ -0,0 +1,8 @@
+"""
+Data Ingestion: Bring data into System
+Data validation: Data range imbalanced Dataset, Outliers, Duplicate data,
+                Schema Validation, Null check, Domain value, Anomalies
+Data Transformation: Performing EDA to understanding data
+Model Training : Model Selection (Model Evaluation)
+                Hyperparameter tuning
+"""
