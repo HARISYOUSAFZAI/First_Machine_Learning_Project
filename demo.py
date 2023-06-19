a46ebc4256b4493d96acc639e52bed3e8b9f3fe7
HARISYOUSAFZAI <HARISALI.510@GMAIL.COM>
2023-06-19 19:23:09 +0300

    ingestion module and pipline related to ingestion is completed

--- demo.py
+++ demo.py
@@ -3,15 +3,16 @@ from housing.pipline.pipeline import Pipeline
 from housing.exception import HousingException
 from housing.logger import logging
 import sys,os
+
+
 def main():
     try:
-
-        pipeline = Pipeline
-        pipeline.run_pipeline()
+        pipe = Pipeline()
+        pipe.run_pipeline()
     except Exception as e:
         logging.error(f"{e}")
         raise HousingException(e,sys) from e
         print(e)    
 
-if __name__ == "__main__":
+if __name__=="__main__":
     main()
